597ebf6a5568cb26a645b700a5c3fc5f228d746b
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-01-07 00:39:22 +0400

    xmenu changes

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+ICONPATH=/home/aditeya/Code/xmenu/icons
+
 cat <<EOF | xmenu | sh &
 Applications
-	IMG:/home/aditeya/.local/share/xmenu/icons/web.png	Web Browser	$BROWSER
-	IMG:/home/aditeya/.local/share/xmenu/icons/thunderbird.png	Thunderbird	thunderbird
-	IMG:/home/aditeya/.local/share/xmenu/icons/gimp.png	Image editor	gimp
+	IMG:$ICONPATH/web.png	Web Browser	$BROWSER
+	IMG:$ICONPATH/thunderbird.png	Thunderbird	thunderbird
+	IMG:$ICONPATH/gimp.png	Image editor	gimp
+	IMG:$ICONPATH/signal.png	Signal	signal-desktop
 Games
-	IMG:/home/aditeya/.local/share/xmenu/icons/multimc.png	Minecraft	multimc
-	IMG:/home/aditeya/.local/share/xmenu/icons/steam.png	Steam	steam
+	IMG:$ICONPATH/multimc.png	Minecraft	multimc
+	IMG:$ICONPATH/steam.png	Steam	lsi-steam
 Terminal (st)		st
 
 Shutdown		poweroff
